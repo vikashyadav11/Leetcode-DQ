a6cbfc27450f034f53c62faa7cf67defad3d66dd
Vikash Yadav <145245994+vikashyadav11@users.noreply.github.com>
2025-03-19 22:16:37 +0530

    Time: 3 ms (89.05%) | Memory: 140.1 MB (15.99%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -5,14 +5,13 @@ public:
         int count=0;
 
         for(int i=0;i<n-2;i++){
-            
-            if(nums[i]==1)
-                continue;
 
-            nums[i]=1;
-            nums[i+1] ^= 1;
-            nums[i+2] ^= 1;
-            count++;
+            if(nums[i]==0){
+                nums[i]=1;
+                nums[i+1] ^= 1;
+                nums[i+2] ^= 1;
+                count++;
+            }
         }
 
         if(nums[n-2]==0 || nums[n-1]==0)
