ad2330d92ce97515715383d00b6556442f235ede
Vikash Yadav <145245994+vikashyadav11@users.noreply.github.com>
2025-07-10 00:14:31 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (79.83%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        if(n==1)    
-            return;
         int left=0;
         for(int right=0;right<n;right++){
             if(nums[right] != 0){
