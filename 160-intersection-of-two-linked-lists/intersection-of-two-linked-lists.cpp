3a0600ff5efdab1fe552a6e236da5ee485a4f27b
Vikash Yadav <145245994+vikashyadav11@users.noreply.github.com>
2025-08-05 01:05:31 +0530

    Time: 30 ms (93.40%) | Memory: 18.6 MB (53.06%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,26 +9,26 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* tempA=headA;
-        ListNode* tempB=headB;
-        ListNode* inter=NULL;
-        while(tempA!=NULL){
-            tempA->val=-(tempA->val);
-            tempA=tempA->next;
-        }
-        while(tempB!=NULL){
-            if(tempB->val < 0){
-                tempB->val=-(tempB->val);
-                inter=tempB;
-                break;
+        
+        ListNode* pointerA = headA; // Initialize pointer A to the head of list A. It's one of our runners.
+        ListNode* pointerB = headB; // Initialize pointer B to the head of list B. This is our second runner.
+
+        while (pointerA != pointerB) { // Keep going until our runners meet! Or until we're absolutely sure they won't meet.
+            if (pointerA == nullptr) { // If runner A reaches the end of its track...
+                pointerA = headB; // ...it starts running on track B! It's like saying "Okay, let's see if we can meet if I run the other way".
+            } else {
+                pointerA = pointerA->next; // Otherwise, runner A continues to the next node on its track. Just keep running!
             }
-            tempB=tempB->next;
+
+            if (pointerB == nullptr) { // Same logic as above, but for runner B. If runner B reaches the end of its track...
+                pointerB = headA; // ...it starts running on track A!
+            } else {
+                pointerB = pointerB->next; // Otherwise, runner B continues to the next node on its track.
+            }
         }
-        tempA=headA;
-        while(tempA!=NULL){
-            if(tempA->val < 0)  tempA->val=-(tempA->val);
-            tempA=tempA->next;
-        }
-        return inter;
+
+        return pointerA; // If the runners meet (pointerA == pointerB), we've found the intersection! Return that node. If the lists don't intersect at all, both runners will be null, and we return null.
     }
+
+    
 };
