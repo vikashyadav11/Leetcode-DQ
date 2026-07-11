3cbee70d8c068ea0563a4e62c215b30d4bd5313c
Vikash Yadav <145245994+vikashyadav11@users.noreply.github.com>
2026-07-11 22:42:23 +0530

    Time: 1143 ms (99.00%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT p.product_name,s.year,s.price FROM Sales AS s LEFT JOIN Product AS p ON s.product_id=p.product_id;
+select product_name,year,price from Sales s left join Product p on s.product_id=p.product_id;
